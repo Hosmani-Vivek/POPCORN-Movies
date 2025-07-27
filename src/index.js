91fc9f625541819571dbdebe91b53f0af8c80390
Vivek Hosmani <159860990+Hosmani-Vivek@users.noreply.github.com>
2025-07-27 15:22:23 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,23 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-//import StarRating from "./StarRating";
 
-/*function Test() {
-  const [MR, SMR] = useState(0);
-  return (
-    <div>
-      <StarRating colour="blue" onSR={SMR} MaxStars={10} />
-      <p>you have rated {MR} stars</p>
-    </div>
-  );
-}*/
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-
-    {/*<Test />*/}
   </React.StrictMode>
 );
